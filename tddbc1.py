ee13af675a29b34d12dd103e5a7d8667b80e39da
kashewnuts <bjzli.m08vo9kqs@gmail.com>
2013-10-12 13:56:28 +0900

    create upper_endpoint

--- tddbc1.py
+++ tddbc1.py
@@ -13,12 +13,21 @@ class TestTddbc1(unittest.TestCase):
         i = Interval(9)
         self.assertEqual(i.lower(), 9)
 
+    def test_lower_endpoint_8_3(self):
+        i = Interval(8, 3)
+        self.assertEqual(i.lower(), 8)
+        self.assertEqual(i.upper(), 3)
+
 class Interval(object):
-    def __init__(self, lower):
+    def __init__(self, lower, upper=None):
         self._lower = lower
+        self._upper = upper
 
     def lower(self):
         return self._lower
+
+    def upper(self):
+        return self._upper
 
 if __name__ == '__main__':
     unittest.main()

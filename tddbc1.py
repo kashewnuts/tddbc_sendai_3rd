f843970568198a072afb4d7e2d6281b78f84dba3
kashewnuts <bjzli.m08vo9kqs@gmail.com>
2013-10-12 14:16:20 +0900

    refactoring

--- tddbc1.py
+++ tddbc1.py
@@ -5,13 +5,16 @@ import unittest
 
 class TestTddbc1(unittest.TestCase):
 
-    def test_lower_endpoint_8(self):
-        i = Interval(8)
-        self.assertEqual(i.lower(), 8)
+    def test_lower_endpoint(self):
+        self.assertRaises(TypeError, Interval, ())
 
-    def test_lower_endpoint_9(self):
-        i = Interval(9)
+    def test_lower_endpoint_8(self):
+        self.assertRaises(TypeError, Interval, (8))
+
+    def test_lower_endpoint_9_4(self):
+        i = Interval(9, 4)
         self.assertEqual(i.lower(), 9)
+        self.assertEqual(i.upper(), 4)
 
     def test_lower_endpoint_8_3(self):
         i = Interval(8, 3)
@@ -19,7 +22,7 @@ class TestTddbc1(unittest.TestCase):
         self.assertEqual(i.upper(), 3)
 
 class Interval(object):
-    def __init__(self, lower, upper=None):
+    def __init__(self, lower, upper):
         self._lower = lower
         self._upper = upper
 

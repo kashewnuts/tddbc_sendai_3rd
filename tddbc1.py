b6e8340f4fba69f88927f826a3a3b09c0ef5ff6d
kashewnuts <bjzli.m08vo9kqs@gmail.com>
2013-10-12 13:45:53 +0900

    traiangulation

--- tddbc1.py
+++ tddbc1.py
@@ -5,16 +5,20 @@ import unittest
 
 class TestTddbc1(unittest.TestCase):
 
-    def test_lower_endpoint_3_8(self):
+    def test_lower_endpoint_8(self):
         i = Interval(8)
         self.assertEqual(i.lower(), 8)
 
+    def test_lower_endpoint_9(self):
+        i = Interval(9)
+        self.assertEqual(i.lower(), 9)
+
 class Interval(object):
     def __init__(self, lower):
-        pass
+        self.l = lower
 
     def lower(self):
-        return 8
+        return self.l
 
 if __name__ == '__main__':
     unittest.main()
